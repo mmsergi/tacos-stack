5aa12b60cbd04a944a067d3c35450751d292c3c8
Sergi <sergimrtnz@gmail.com>
2019-11-12 10:53:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-var db = firebase.firestore();
+
+// test
 
 // listado de productos de nuestra web
 var productos = [
